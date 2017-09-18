c5750cd1f56bad8430e463b13dd1d25ed1a4d366
MartijnSmit1 <martijnie4132@gmail.com>
2017-09-18 13:34:01 +0200

    Bootcamp
    Bootcamp

---
+++ opdracht 5 en 6.js
@@ -0,0 +1,17 @@
+const Number1 = 7;
+const Number2 = 8;
+const Number3 = 9;
+const Tekst4 = "is" ;
+const Tekst5 = "dit";
+const Tekst6 = "tekst";
+const Tekst7 = "?";
+const juist = false;
+const fout = true;
+const Number10 = 20;
+const Number11 = 19;
+const Number12 = 18;
+const Number13 = 14;
+const Number14 = 15;
+const Number15 = 16;
+
+typeof const Number15;
---
+++ opdracht2 3 en 4.js
@@ -0,0 +1,16 @@
+const Number1 = 1;
+const Number2 = 2;
+const Number3 = 3;
+const Tekst4 = "Dit" ;
+const Tekst5 = "is";
+const Tekst6 = "een";
+const Tekst7 = "tekst";
+const juist = true;
+const fout = false;
+const Number10 = 10;
+const Number11 = 11;
+const Number12 = 12;
+const Number13 = 13;
+const Number14 = 14;
+const Number15 = 15;
+
